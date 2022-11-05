513915bfe7c90c7bbe156337bb7cf58fb1b1f66a
Mario A. Chacano <64982811+mariochacano@users.noreply.github.com>
2022-11-05 18:10:15 -0300

    Update Example1.R

--- Example1.R
+++ Example1.R
@@ -1,5 +1,21 @@
-#Parameters
-.beta=1.768252			#shape
-.eta=49.650508			#scale
-.x <- seq(0, 150)		#x axis
-t=50				        #time
+#Load Packages
+library(MASS)
+library(survival)
+library(fitdistrplus)
+library(tidyr)
+
+# Data
+Dataset <- c(110,120,220,220,220,330)
+
+#Parameters from fitting
+swp <- fitdist(Dataset, dist="weibull")
+.beta <- as.numeric(swp$estimate["shape"])	#shape
+.eta <- as.numeric(swp$estimate["scale"])		#scale
+.gamma <- 0                                 #location
+
+
+# FUNCTIONS
+R <- function(t){exp(-(((t-.gamma)/.eta)^(.beta)))}
+F <- function(t){1-R(t)}
+H <- function(t){(.beta/.eta)*((t/.eta)^(.beta-1))}
+WeibullMean <- function(.beta,.eta,.gamma){.gamma+.eta*gamma(1+(1/.beta))}
