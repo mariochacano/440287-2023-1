f6190d498d5cfff3074b57f09ecded78e1bdc15a
Mario A. Chacano <64982811+mariochacano@users.noreply.github.com>
2022-11-06 13:22:35 -0300

    Update Example1.R

--- Example1.R
+++ Example1.R
@@ -7,10 +7,10 @@ library(fitdistrplus)
 library(tidyr)
 library(dplyr)
 
-# Data
+# Data for example
 Dataset <- c(110,120,220,220,220,330)
 
-#Parameters from fitting
+#Parameters from fitting weibull MLE
 fwp <- function(x){
   swp <- fitdist(x, dist="weibull")
   .beta <- as.numeric(swp$estimate["shape"])	#shape
@@ -23,12 +23,13 @@ fwp <- function(x){
   return(result)
 }
 
-wp <- fwp(Dataset)
-
-# FUNCTIONS
+# Weibull Distribution Functions
 R <- function(t,wp){exp(-(((t-wp$location)/wp$scale)^(wp$shape)))}
 F <- function(t,wp){1-R(t,wp)}
 H <- function(t,wp){(wp$shape/wp$scale)*((t/wp$scale)^(wp$shape-1))}
 WeibullMean <- function(wp){wp$location+wp$scale*gamma(1+(1/wp$shape))}
 
+# Some Results
+wp <- fwp(Dataset)
 WeibullMean(wp)
+R(0,wp)
