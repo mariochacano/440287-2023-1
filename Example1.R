c41495e70499262a8e37dedf99de8e4d00aade30
Mario A. Chacano <64982811+mariochacano@users.noreply.github.com>
2023-02-21 17:17:11 -0300

    Update Example1.R

--- Example1.R
+++ Example1.R
@@ -1,24 +1,12 @@
 #Load Packages
 library(tidyverse)
-library(fitdistrplus)
 
 # Load Weibull Functions
+source(paste(getwd(),"/MaintenanceFunctions.R", sep=""))
 source(paste(getwd(),"/WeibullFunctions.R", sep=""))
-
-# Parameters from fitting weibull MLE
-fwp <- function(x){
-  swp <- fitdist(x, dist="weibull")
-  .beta <- as.numeric(swp$estimate["shape"])	#shape
-  .eta <- as.numeric(swp$estimate["scale"])		#scale
-  .gamma <- 0                                 #location
-  print(paste("shape =",.beta))
-  print(paste("scale =",.eta))
-  print(paste("location =",.gamma))
-  result <- list("shape"=.beta, "scale"=.eta, "location"=.gamma)
-  return(result)
-}
+source(paste(getwd(),"/Fit_Weibull_Parameters.R", sep=""))
 
 # Example 1
-Dataset <- c(110,120,220,220,220,330)
-wp <- fwp(Dataset)
-(MTBF <- WMean(wp))
+df2 <- data.frame(tbfs=c(110,330,120,220,220,220))
+print(df2$tbfs %>% fit_Wp_MLE())
+WMTBF <- df2$tbfs %>% fit_Wp_MLE() %>% WMean()
