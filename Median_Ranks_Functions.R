5a64f9c7c6658fe6e49077a2b358922b191adcdb
Mario A. Chacano <64982811+mariochacano@users.noreply.github.com>
2023-02-21 16:49:51 -0300

    Create Median_Ranks_Functions.R

---
+++ Median_Ranks_Functions.R
@@ -0,0 +1,26 @@
+# ALL FUNCTIONS ARE DESCRIBED BY A DATAFRAME "DF" WITH ONE COLUMN DATA WICH ARE TBFS OR TTRS AND RETURN WITH A NEW COLUMN CALLED MR
+
+i_kaesimo_MR <- function(df){
+  n <- nrow(df)
+  df %>% 
+    mutate("pos"=as.numeric(rownames(.))) %>%
+    mutate("MR"=(pos/(n+1))) %>% 
+    dplyr::select(-pos)
+  }
+
+bernard_RRX_MR <- function(df){
+  n <- nrow(df)
+  df %>% 
+    mutate("pos"=as.numeric(rownames(.))) %>%
+    mutate("MR"=((pos-0.3)/(n+0.4))) %>% 
+    dplyr::select(-pos)
+  }
+
+kaplan_meier_MR <- function(df){
+  n <- nrow(df)
+  df %>% 
+    mutate("pos"=as.numeric(rownames(.))) %>%
+    mutate("y1"=(((n+0.7)/(n+0.4))*((n-pos+0.7)/(n-pos+1.7))),
+           "MR" = accumulate(y1, ~ 1-.y*(1-.x), .init = 0)[-1]) %>% 
+    dplyr::select(-y1,-pos)
+}
